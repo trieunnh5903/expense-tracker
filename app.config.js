a8b30c828e0e325803a79197135d34ac00c004a7
trieunnh5903 <nguyennhathaitrieu5903@gmail.com>
2026-08-09 12:33:04 +0700

    feat: initital project

---
+++ app.config.js
@@ -0,0 +1,10 @@
+
+module.exports = ({ config }) => {
+    return {
+        ...config,
+        android: {
+            ...config.android,
+            googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? './google-services.json',
+        },
+    };
+};
